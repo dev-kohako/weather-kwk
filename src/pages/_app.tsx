fb5a20cadd466497a01a268d23d0d95d208627a7
kohako_dev <kohako.tech@gmail.com>
2025-03-27 04:09:08 -0300

    feat: creation of the config to apollo provider and next themes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,36 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { Lexend } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
+
+const lexend = Lexend({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const client = new ApolloClient({
+    uri: "http://localhost:4000/graphql",
+    cache: new InMemoryCache(),
+    defaultOptions: {
+      watchQuery: { fetchPolicy: "network-only" },
+      query: { fetchPolicy: "network-only" },
+    },
+  });
+
+  return (
+    <main className={lexend.className}>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <ApolloProvider client={client}>
+          <Component {...pageProps} />
+        </ApolloProvider>
+      </ThemeProvider>
+    </main>
+  );
 }
