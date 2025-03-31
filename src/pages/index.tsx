7f181ed3ef659b674c6ae6f52e7ebfd378cd11b0
kohako_dev <kohako.tech@gmail.com>
2025-03-31 13:46:12 -0300

    feat: adding the earth component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,45 +1,27 @@
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import GlobeComponent from "../components/earth-3d/GlobeWrapper";
 
 export default function Home() {
+
   const dataOff = [
-    {
-      id: 1,
-      name: "1",
-    },
-    {
-      id: 2,
-      name: "2",
-    },
-    {
-      id: 3,
-      name: "3",
-    },
-    {
-      id: 4,
-      name: "4",
-    },
-    {
-      id: 5,
-      name: "5",
-    },
-    {
-      id: 6,
-      name: "6",
-    },
-    {
-      id: 7,
-      name: "7",
-    },
+    { id: 1, name: "1", minTemp: 17, temp: 24, maxTemp: 29 },
+    { id: 2, name: "2", minTemp: 17, temp: 24, maxTemp: 29 },
+    { id: 3, name: "3", minTemp: 17, temp: 24, maxTemp: 29 },
+    { id: 4, name: "4", minTemp: 17, temp: 24, maxTemp: 29 },
+    { id: 5, name: "5", minTemp: 17, temp: 24, maxTemp: 29 },
+    { id: 6, name: "6", minTemp: 17, temp: 24, maxTemp: 29 },
+    { id: 7, name: "7", minTemp: 17, temp: 24, maxTemp: 29 },
   ];
 
   return (
-    <div className="flex flex-col justify-center items-center bg-zinc-300 dark:bg-zinc-900">
+    <div className="flex flex-col justify-center items-center bg-zinc-300 dark:bg-zinc-900 overflow-hidden">
       <Header />
-      <div className="min-h-screen flex flex-col justify-center items-center h-screen w-full max-w-7xl space-y-10 px-4 lg:px-0">
-        <div className="w-full columns-2">
-          <div className="bg-zinc-950 sm:w-[30rem] h-[30rem] self-start"></div>
-          <div className="bg-zinc-950 h-[30rem] p-4"></div>
+      <div className="min-h-screen flex flex-col justify-center items-center h-screen w-full max-w-7xl space-y-10 lg:px-0">
+        <div className="flex flex-col h-[390px] w-[390px] justify-center items-center">
+          <div className="flex w-full h-full justify-center items-center">
+            <GlobeComponent />
+          </div>
         </div>
 
         <div className="w-full flex justify-between items-center">
